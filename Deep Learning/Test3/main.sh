6a9a9713768a559f2f7c107e48ea70e426b4d2f0
pedrum <pedrum.jalali@gmail.com>
2017-10-14 18:37:50 -0400

    f

--- Deep Learning/Test3/main.sh
+++ Deep Learning/Test3/main.sh
@@ -11,19 +11,19 @@ ip=127.0.0.1
 db=insta
 hidden_layers=200
 rnn_length=5
-ratio=0.01
+ratio=10
 test_ratio=0.3
 train_count=10000
 model=1
 
 
-for train_count in 1000 10000
+for train_count in 100 
 do
-  for hidden_layers in 400 700 
+  for hidden_layers in 400  
   do
     for drop_out in 0.5
     do
-      python Main.py $pass $ip $db $hidden_layers $rnn_length $ratio $test_ratio $train_count $model > output/${train_count}_${hidden_layers}.txt 
+      python Main.py $pass $ip $db $hidden_layers $rnn_length $ratio $test_ratio $train_count $model #> output/${train_count}_${hidden_layers}.txt 
     done
   done
 done
